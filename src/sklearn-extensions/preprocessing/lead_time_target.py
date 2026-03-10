2a526590ea8cfeb6d5c4e40081c9b8e3147e8707
eugenioLR <eugeniolorente0@gmail.com>
2026-03-10 16:58:39 +0100

    added lagged feature transformer and lead time transformer

---
+++ src/sklearn-extensions/preprocessing/lead_time_target.py
@@ -0,0 +1,39 @@
+from sklearn.base import BaseEstimator, TransformerMixin
+
+class LeadFeatureTransformer(BaseEstimator, TransformerMixin):
+    def __init__(self, lead_time, flatten_output=True, apply_mask=False):
+        self.lead_time = lead_time
+        self.flatten_output = flatten_output
+        self.apply_mask = apply_mask
+
+    def fit(self, X, y=None, **fit_params):
+        return self
+
+    def transform(self, X):
+        ntimesteps = X.shape[0]
+        nfeatures = X.shape[1]
+
+        self.mask_ = np.arange(ntimesteps) >= self.lead_time
+
+        windowed_features = np.full((ntimesteps, nfeatures, self.lead_time), np.nan)
+        for idx, val in enumerate(X_ts[:-self.lead_time]):
+            windowed_features[idx + self.lead_time] = X[idx : idx + self.lead_time, :]
+        
+        if self.flatten_output:
+            windowed_features = windowed_features.view((ntimesteps, nfeatures * self.lead_time))
+        if self.apply_mask:
+            windowed_features = windowed_features[self.mask_, :]
+
+        return windowed_features
+    
+    def transform_target(self, y):
+        if self.apply_mask:
+            y_result = y[self.lead_time:]
+        else:
+            y_result = self.full_like(y, np.nan)
+            y_result[self.lead_time:] = y[:-self.lead_time]
+
+        return y_result
+    
+    def transform_dataset(self, X, y):
+        return self.transform(X), self.transform_target(y)
