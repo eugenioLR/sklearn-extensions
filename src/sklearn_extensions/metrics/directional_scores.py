b855c6093823baf32df24a1e9c872d2da2cc8a09
eugenioLR <eugeniolorente0@gmail.com>
2026-04-17 19:33:58 +0200

    added input validation to circular metrics

--- src/sklearn_extensions/metrics/directional_scores.py
+++ src/sklearn_extensions/metrics/directional_scores.py
@@ -12,6 +12,10 @@ def circular_absolute_error(y_true, y_pred):
     return np.mean(np.minimum(pred_abs_diff, period - pred_abs_diff))
 
 def circular_root_mean_square_error(y_true, y_pred):
+    y_true = check_array(y_true, ensure_2d=False)
+    y_pred = check_array(y_pred, ensure_2d=False)
+    check_consistent_length(y_true, y_pred)
+
     period = 2*np.pi
     pred_abs_diff = np.abs(y_true - y_pred)
     angular_diff = np.minimum(pred_abs_diff, period - pred_abs_diff)
@@ -19,6 +23,10 @@ def circular_root_mean_square_error(y_true, y_pred):
     return np.sqrt(np.mean(angular_diff**2))
 
 def circular_bias(y_true, y_pred):
+    y_true = check_array(y_true, ensure_2d=False)
+    y_pred = check_array(y_pred, ensure_2d=False)
+    check_consistent_length(y_true, y_pred)
+
     period = 2*np.pi
     wrapped_angle_diff = (y_true - y_pred) % period
     mean_cos = np.cos(wrapped_angle_diff)
